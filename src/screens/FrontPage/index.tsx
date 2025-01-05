74c8d7d6b36787d06b7d4950f6b9f3c4a5f5d02f
11Some1 <68611574+11Some1@users.noreply.github.com>
2025-01-04 20:14:56 -0500

    fix auth access

--- src/screens/FrontPage/index.tsx
+++ src/screens/FrontPage/index.tsx
@@ -6,8 +6,11 @@ import DaliDarkImg from '@/assets/dali_dark.png';
 import { SERVER_URL } from '@/utils/constants';
 import { logout } from '@/api/auth';
 import useBoundStore from '@/store';
+import { getAuthUser, initCredentials, jwtSignIn } from '@/api/auth';
 
 function FrontPage() {
+  const { authenticated, role } = getAuthUser().data;
+
   const { mutate: mutateLogout } = logout();
   const bearCount = useBoundStore((state) => state.bearCount);
   const fishCount = useBoundStore((state) => state.fishCount);
@@ -27,27 +30,37 @@ function FrontPage() {
       <div>
         Using SERVER_URL = {SERVER_URL}
       </div>
-      <Link to={ROUTES.SIGNIN}>
-        <h1>Sign In</h1>
-      </Link>
-      <Link to={ROUTES.SIGNUP}>
-        <h1>Sign Up</h1>
-      </Link>
-      <Link to={ROUTES.VERIFY}>
-        <h1>Verify</h1>
-      </Link>
-      <Link to={ROUTES.USERS}>
-        <h1>Users (admin only)</h1>
-      </Link>
-      <Link to={ROUTES.RESOURCES}>
-        <h1>Resources (user or admin)</h1>
-      </Link>
-      <button onClick={() => mutateLogout()}>Logout</button>
-      <div>
-        (bears, fish) = ({bearCount}, {fishCount})
-      </div>
-      <button onClick={() => addBear()}>Add bear</button>
-      <button onClick={() => addFish()}>Add fish</button>
+      {
+        !authenticated && 
+          <>
+            <Link to={ROUTES.SIGNIN}>
+              <h1>Sign In</h1>
+            </Link>
+            <Link to={ROUTES.SIGNUP}>
+              <h1>Sign Up</h1>
+            </Link>
+            <Link to={ROUTES.VERIFY}>
+              <h1>Verify</h1>
+            </Link>
+          </>
+      }
+      {
+        authenticated &&
+        <>
+          <Link to={ROUTES.USERS}>
+            <h1>Users (admin only)</h1>
+          </Link>
+          <Link to={ROUTES.RESOURCES}>
+            <h1>Resources (user or admin)</h1>
+          </Link>
+          <button onClick={() => mutateLogout()}>Logout</button>
+          <div>
+            (bears, fish) = ({bearCount}, {fishCount})
+          </div>
+          <button onClick={() => addBear()}>Add bear</button>
+          <button onClick={() => addFish()}>Add fish</button>
+        </>
+      }
     </div>
   );
 }
