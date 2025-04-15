fcabbf966462d7e54789172c21f27b03e75b0025
miruna <58366207+mirunaish@users.noreply.github.com>
2025-04-15 04:40:48 -0400

    improved dashboard

--- src/screens/DashboardPage/NavWidget.tsx
+++ src/screens/DashboardPage/NavWidget.tsx
@@ -1,17 +1,11 @@
-import { Link } from "react-router-dom";
 import "./styles.scss";
 
 interface NavWidgetProps {
-  to: string;
   children: React.ReactNode;
 }
 
-const NavWidget = ({ to, children }: NavWidgetProps) => {
-  return (
-    <Link to={to}>
-      <div className="nav-widget">{children}</div>
-    </Link>
-  );
+const NavWidget = ({ children }: NavWidgetProps) => {
+  return <div className="nav-widget">{children}</div>;
 };
 
 export default NavWidget;
