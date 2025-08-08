2d2ff0441069988128e9b19bb7975f24d9e03643
miruna <58366207+mirunaish@users.noreply.github.com>
2025-08-07 23:39:37 -0400

    export

---
+++ src/api/export.ts
@@ -0,0 +1,22 @@
+import { SERVER_URL } from "@/utils/constants";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+
+export const getExport = () => {
+  return useMutation({
+    mutationFn: async (): Promise<Blob> => {
+      return axios
+        .post(`${SERVER_URL}export/`, null, { responseType: "blob" })
+        .then((response) => {
+          return response.data;
+        })
+        .catch((error) => {
+          throw Error(error.response?.data?.errors.join("; ") ?? error);
+        });
+    },
+    onError: (error: Error) => {
+      console.error("Export failed:", error.message);
+      throw error;
+    },
+  });
+};
